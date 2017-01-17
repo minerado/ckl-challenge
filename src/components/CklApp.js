b43e5f81f63a0c9693c18e42c25a0eb69aec3b49
Unknown <ramondiogo10.1@gmail.com>
2017-01-17 14:08:47 -0200

    Add header classnames

--- src/components/CklApp.js
+++ src/components/CklApp.js
@@ -3,13 +3,13 @@ import React from 'react'
 
 const CklApp = () => (
     <div>
-        <header>
-            <div>
-                <div>
+        <header className='header'>
+            <div className='header-wrapper'>
+                <div className='header-menu'>
                     Menu
                 </div>
                 
-                <nav>
+                <nav className='header-items'>
                     <div>
                         POLITICS
                     </div>
