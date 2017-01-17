51c58994899b36ce347988f2a95931e1d3ab5428
Unknown <ramondiogo10.1@gmail.com>
2017-01-17 16:17:11 -0200

    Add initial post structure

--- src/components/CklApp.js
+++ src/components/CklApp.js
@@ -29,8 +29,179 @@ const CklApp = () => (
             </div>
         </header>
 
+        
         <main>
-            App's main body (here goes all the posts)
+            <section className='posts-top'>
+                <article className='post'>
+                    <div className='post-tag'>
+                        TAG
+                    </div>
+
+                    <div className='post-image'>
+                        IMAGE
+                    </div>
+
+                    <div className="post-title">
+                        POST TITLE
+                    </div>
+
+                    <div className="post-author">
+                        <div className="post-author__image">
+                            AUTHOR IMAGE
+                        </div>
+
+                        <div className="post-author__name">
+                            by AUTHOR NAME
+                        </div>
+                    </div>
+
+                    <div className="post-content line-clamp-8">
+                        POST TEXT
+                    </div>
+                </article>
+
+                <article className='post'>
+                    <div className='post-tag'>
+                        TAG
+                    </div>
+
+                    <div className='post-image'>
+                        IMAGE
+                    </div>
+
+                    <div className="post-title">
+                        POST TITLE
+                    </div>
+
+                    <div className="post-author">
+                        <div className="post-author__image">
+                            AUTHOR IMAGE
+                        </div>
+
+                        <div className="post-author__name">
+                            by AUTHOR NAME
+                        </div>
+                    </div>
+
+                    <div className="post-content line-clamp-8">
+                        POST TEXT
+                    </div>
+                </article>
+
+                <article className='post'>
+                    <div className='post-tag'>
+                        TAG
+                    </div>
+
+                    <div className='post-image'>
+                        IMAGE
+                    </div>
+
+                    <div className="post-title">
+                        POST TITLE
+                    </div>
+
+                    <div className="post-author">
+                        <div className="post-author__image">
+                            AUTHOR IMAGE
+                        </div>
+
+                        <div className="post-author__name">
+                            by AUTHOR NAME
+                        </div>
+                    </div>
+
+                    <div className="post-content line-clamp-8">
+                        POST TEXT
+                    </div>
+                </article>
+            </section>
+
+            <section className='posts-bottom'>
+                <article className='post'>
+                    <div className='post-tag'>
+                        TAG
+                    </div>
+
+                    <div className='post-image'>
+                        IMAGE
+                    </div>
+
+                    <div className="post-title">
+                        POST TITLE
+                    </div>
+
+                    <div className="post-author">
+                        <div className="post-author__image">
+                            AUTHOR IMAGE
+                        </div>
+
+                        <div className="post-author__name">
+                            by AUTHOR NAME
+                        </div>
+                    </div>
+
+                    <div className="post-content line-clamp-8">
+                        POST TEXT
+                    </div>
+                </article>
+
+                <article className='post'>
+                    <div className='post-tag'>
+                        TAG
+                    </div>
+
+                    <div className='post-image'>
+                        IMAGE
+                    </div>
+
+                    <div className="post-title">
+                        POST TITLE
+                    </div>
+
+                    <div className="post-author">
+                        <div className="post-author__image">
+                            AUTHOR IMAGE
+                        </div>
+
+                        <div className="post-author__name">
+                            by AUTHOR NAME
+                        </div>
+                    </div>
+
+                    <div className="post-content line-clamp-8">
+                        POST TEXT
+                    </div>
+                </article>
+
+                <article className='post'>
+                    <div className='post-tag'>
+                        TAG
+                    </div>
+
+                    <div className='post-image'>
+                        IMAGE
+                    </div>
+
+                    <div className="post-title">
+                        POST TITLE
+                    </div>
+
+                    <div className="post-author">
+                        <div className="post-author__image">
+                            AUTHOR IMAGE
+                        </div>
+
+                        <div className="post-author__name">
+                            by AUTHOR NAME
+                        </div>
+                    </div>
+
+                    <div className="post-content line-clamp-8">
+                        POST TEXT
+                    </div>
+                </article>
+            </section>
         </main>
     </div>
 )
