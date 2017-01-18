6f285359773acdb3fd5f0586b3247769c16ce710
Unknown <ramondiogo10.1@gmail.com>
2017-01-18 11:17:20 -0200

    Add Post component to be rendered in the PostList component with the db

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,178 +1,30 @@
 import React from 'react'
 
+import DB from '../db'
+import { separatePosts } from '../libs/post_lib'
+
+import Post from './Post'
+
+const renderPost = (post) => (
+    <Post
+        title={post.title}
+        text={post.text}
+        image={post.image}
+        tag={post.tag}
+        author_name={post.user.name}
+        author_image={post.user.image}
+        key={post.id}
+    />
+)
 
 const PostList = () => (
     <main>
         <section className='posts-top'>
-            <article className='post'>
-                <div className='post-tag post-tag-politics'>
-                    POLITICS
-                </div>
-
-                <div className='post-image'
-                        style={{
-                            backgroundImage: `url("http://s2.glbimg.com/k9nnD3e1icw7wKYe0dBLd3f8SZA=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/B/l/tnXrCAQkCdIkQP6GInRg/trump-13-01.jpg")`,
-                        }}>
-                </div>
-
-                <div className="post-title">
-                    POST TITLE
-                </div>
-
-                <div className="post-author">
-                    <div className="post-author__image"></div>
-
-                    <div className="post-author__name">
-                        by AUTHOR NAME
-                    </div>
-                </div>
-
-                <div className="post-content line-clamp-8">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut velit dui. Fusce mauris ex, fringilla eget placerat vitae, euismod vel ligula. Sed sed tortor id neque finibus molestie sed ac justo. Pellentesque in tincidunt diam, a porta massa. In ac nunc aliquet, auctor nulla varius, ornare nisi. Quisque blandit urna a dui scelerisque, non facilisis massa fermentum. Donec sagittis dui sodales odio ultricies ornare. Curabitur mattis sed leo a finibus. Etiam hendrerit auctor congue. Integer vel ex maximus, fermentum lacus ac, tristique enim. In laoreet tellus non sodales consectetur.
-                </div>
-            </article>
-
-            <article className='post'>
-                <div className='post-tag post-tag-business'>
-                    BUSINESS
-                </div>
-
-                <div className='post-image'
-                        style={{
-                            backgroundImage: `url("http://s2.glbimg.com/k9nnD3e1icw7wKYe0dBLd3f8SZA=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/B/l/tnXrCAQkCdIkQP6GInRg/trump-13-01.jpg")`,
-                        }}>
-                </div>
-
-                <div className="post-title">
-                    POST TITLE
-                </div>
-
-                <div className="post-author">
-                    <div className="post-author__image"></div>
-
-                    <div className="post-author__name">
-                        by AUTHOR NAME
-                    </div>
-                </div>
-
-                <div className="post-content line-clamp-8">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut velit dui. Fusce mauris ex, fringilla eget placerat vitae, euismod vel ligula. Sed sed tortor id neque finibus molestie sed ac justo. Pellentesque in tincidunt diam, a porta massa. In ac nunc aliquet, auctor nulla varius, ornare nisi. Quisque blandit urna a dui scelerisque, non facilisis massa fermentum. Donec sagittis dui sodales odio ultricies ornare. Curabitur mattis sed leo a finibus. Etiam hendrerit auctor congue. Integer vel ex maximus, fermentum lacus ac, tristique enim. In laoreet tellus non sodales consectetur.
-                </div>
-            </article>
-
-            <article className='post'>
-                <div className='post-tag post-tag-tech'>
-                    TECH
-                </div>
-
-                <div className='post-image'
-                        style={{
-                            backgroundImage: `url("http://s2.glbimg.com/k9nnD3e1icw7wKYe0dBLd3f8SZA=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/B/l/tnXrCAQkCdIkQP6GInRg/trump-13-01.jpg")`,
-                        }}>
-                </div>
-
-                <div className="post-title">
-                    POST TITLE
-                </div>
-
-                <div className="post-author">
-                    <div className="post-author__image"></div>
-
-                    <div className="post-author__name">
-                        by AUTHOR NAME
-                    </div>
-                </div>
-
-                <div className="post-content line-clamp-8">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut velit dui. Fusce mauris ex, fringilla eget placerat vitae, euismod vel ligula. Sed sed tortor id neque finibus molestie sed ac justo. Pellentesque in tincidunt diam, a porta massa. In ac nunc aliquet, auctor nulla varius, ornare nisi. Quisque blandit urna a dui scelerisque, non facilisis massa fermentum. Donec sagittis dui sodales odio ultricies ornare. Curabitur mattis sed leo a finibus. Etiam hendrerit auctor congue. Integer vel ex maximus, fermentum lacus ac, tristique enim. In laoreet tellus non sodales consectetur.
-                </div>
-            </article>
+            {separatePosts(DB).main_posts.map((p, index) => renderPost(p))}
         </section>
 
         <section className='posts-bottom'>
-            <article className='post'>
-                <div className='post-tag post-tag-science'>
-                    SCIENCE
-                </div>
-
-                <div className='post-image'
-                        style={{
-                            backgroundImage: `url("http://s2.glbimg.com/k9nnD3e1icw7wKYe0dBLd3f8SZA=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/B/l/tnXrCAQkCdIkQP6GInRg/trump-13-01.jpg")`,
-                        }}>
-                </div>
-
-                <div className="post-title">
-                    POST TITLE
-                </div>
-
-                <div className="post-author">
-                    <div className="post-author__image"></div>
-
-                    <div className="post-author__name">
-                        by AUTHOR NAME
-                    </div>
-                </div>
-
-                <div className="post-content line-clamp-8">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut velit dui. Fusce mauris ex, fringilla eget placerat vitae, euismod vel ligula. Sed sed tortor id neque finibus molestie sed ac justo. Pellentesque in tincidunt diam, a porta massa. In ac nunc aliquet, auctor nulla varius, ornare nisi. Quisque blandit urna a dui scelerisque, non facilisis massa fermentum. Donec sagittis dui sodales odio ultricies ornare. Curabitur mattis sed leo a finibus. Etiam hendrerit auctor congue. Integer vel ex maximus, fermentum lacus ac, tristique enim. In laoreet tellus non sodales consectetur.
-                </div>
-            </article>
-
-            <article className='post'>
-                <div className='post-tag post-tag-sports'>
-                    SPORTS
-                </div>
-
-                <div className='post-image'
-                        style={{
-                            backgroundImage: `url("http://s2.glbimg.com/k9nnD3e1icw7wKYe0dBLd3f8SZA=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/B/l/tnXrCAQkCdIkQP6GInRg/trump-13-01.jpg")`,
-                        }}>
-                </div>
-
-                <div className="post-title">
-                    POST TITLE
-                </div>
-
-                <div className="post-author">
-                    <div className="post-author__image"></div>
-
-                    <div className="post-author__name">
-                        by AUTHOR NAME
-                    </div>
-                </div>
-
-                <div className="post-content line-clamp-8">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut velit dui. Fusce mauris ex, fringilla eget placerat vitae, euismod vel ligula. Sed sed tortor id neque finibus molestie sed ac justo. Pellentesque in tincidunt diam, a porta massa. In ac nunc aliquet, auctor nulla varius, ornare nisi. Quisque blandit urna a dui scelerisque, non facilisis massa fermentum. Donec sagittis dui sodales odio ultricies ornare. Curabitur mattis sed leo a finibus. Etiam hendrerit auctor congue. Integer vel ex maximus, fermentum lacus ac, tristique enim. In laoreet tellus non sodales consectetur.
-                </div>
-            </article>
-
-            <article className='post'>
-                <div className='post-tag post-tag-politics'>
-                    POLITICS
-                </div>
-
-                <div className='post-image'
-                        style={{
-                            backgroundImage: `url("http://s2.glbimg.com/k9nnD3e1icw7wKYe0dBLd3f8SZA=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/B/l/tnXrCAQkCdIkQP6GInRg/trump-13-01.jpg")`,
-                        }}>
-                </div>
-
-                <div className="post-title">
-                    POST TITLE
-                </div>
-
-                <div className="post-author">
-                    <div className="post-author__image"></div>
-
-                    <div className="post-author__name">
-                        by AUTHOR NAME
-                    </div>
-                </div>
-
-                <div className="post-content line-clamp-8">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut velit dui. Fusce mauris ex, fringilla eget placerat vitae, euismod vel ligula. Sed sed tortor id neque finibus molestie sed ac justo. Pellentesque in tincidunt diam, a porta massa. In ac nunc aliquet, auctor nulla varius, ornare nisi. Quisque blandit urna a dui scelerisque, non facilisis massa fermentum. Donec sagittis dui sodales odio ultricies ornare. Curabitur mattis sed leo a finibus. Etiam hendrerit auctor congue. Integer vel ex maximus, fermentum lacus ac, tristique enim. In laoreet tellus non sodales consectetur.
-                </div>
-            </article>
+            {separatePosts(DB).second_posts.map((p, index) => renderPost(p))}
         </section>
     </main>
 )
