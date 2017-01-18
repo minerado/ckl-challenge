dff239b7bbc5e4c2e37bd381ae04a017eee5b34c
Unknown <ramondiogo10.1@gmail.com>
2017-01-18 18:13:48 -0200

    Add thunk support to store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import createLogger from 'redux-logger'
+import thunk from 'redux-thunk'
 
 import cklApp from './reducers'
 
 
 export default () => {
     const middlewares = [
-        createLogger()
+        createLogger(),
+        thunk,
     ]
 
     return createStore(
