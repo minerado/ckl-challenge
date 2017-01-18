bd93f678f865516cfb79503edaa5f7de25c79b26
Unknown <ramondiogo10.1@gmail.com>
2017-01-18 18:16:44 -0200

    Add action to fetch posts from the api

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,16 @@
+import { getFilteredPosts } from './api'
+
+
+const receivePosts = (posts) => ({
+    posts,
+    type: 'RECEIVE_POSTS',
+})
+
 export const toggleMenu = (menu) => ({
     menu: !menu,
     type: 'TOGGLE_MENU',
 })
+
+export const fetchPosts = (filter) => (dispatch, getState ) => {
+    return getFilteredPosts(filter).then(posts => dispatch(receivePosts(posts)))
+}
