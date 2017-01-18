4b1706e8301faf89379b6cdc61eb5fd318ff39d2
Unknown <ramondiogo10.1@gmail.com>
2017-01-18 18:19:14 -0200

    Remove old filterPosts method, since we're not using it anymore

--- src/libs/post_lib.js
+++ src/libs/post_lib.js
@@ -4,6 +4,3 @@ export const separatePosts = (posts, index = 3) => ({
     main_posts: posts.slice(0, index),
     second_posts: posts.slice(index, posts.length),
 })
-
-export const filterPosts = (posts, filter) =>
-    filter ? posts.filter(p => p.tag.toLowerCase() === filter) : posts
