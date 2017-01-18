0ca71307cd84e1116d13f5ed08c968b2f7fbb1b9
Unknown <ramondiogo10.1@gmail.com>
2017-01-18 11:55:23 -0200

    Add Provider component to encapsulate the app
    With this the children component can access the store and its methods

--- src/components/CklApp.js
+++ src/components/CklApp.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+
+import configureStore from '../store'
 
 import Header from './Header'
 import PostList from './PostList'
 
+const store = configureStore()
 
 const CklApp = () => (
-    <div>
-        <Header />
-        <PostList />
-    </div>
+    <Provider store={store}>
+        <div>
+            <Header />
+            <PostList />
+        </div>
+    </Provider>
 )
 
 
