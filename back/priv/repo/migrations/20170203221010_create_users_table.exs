4ddbcb57a7ccc15a0d46374ba4fc9c5749a1f14e
Unknown <ramondiogo10.1@gmail.com>
2017-02-03 20:18:52 -0200

    Add users table migration

---
+++ back/priv/repo/migrations/20170203221010_create_users_table.exs
@@ -0,0 +1,21 @@
+defmodule CklNews.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def up do
+    execute "
+      CREATE TABLE users(
+        id serial PRIMARY KEY
+        , name text NOT NULL
+        , image text NOT NULL
+        , inserted_at timestamp NOT NULL DEFAULT now()
+        , updated_at timestamp NOT NULL DEFAULT now()
+      );
+    "
+  end
+
+  def down do
+    execute "
+      DROP TABLE users;
+    "
+  end
+end
