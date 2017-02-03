16349109a552fd44f2a328590517e7b1fd5c0ccb
Unknown <ramondiogo10.1@gmail.com>
2017-02-03 20:38:47 -0200

    Add tags table migration

---
+++ back/priv/repo/migrations/20170203221922_create_tags_table.exs
@@ -0,0 +1,22 @@
+defmodule CklNews.Repo.Migrations.CreateTagsTable do
+  use Ecto.Migration
+
+  def up do
+    execute "
+        CREATE TABLE tags(
+          id serial PRIMARY KEY
+          , name text NOT NULL
+        );
+    "
+
+    execute "
+        CREATE UNIQUE INDEX only_lowercase_tag on tags (lower(name));
+    "
+  end
+
+  def down do
+    execute "
+        DROP TABLE tags;
+    "
+  end
+end
