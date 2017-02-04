eda74d63a9cce28d2c098c3cdfdd7086151ca3a7
Unknown <ramondiogo10.1@gmail.com>
2017-02-04 11:07:26 -0200

    Change store initialization to work with new middleware constructor

--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import configureStore from './store'
-
+import store from './store'
 import Root from './components/Root'
 
 import '../style/bundle.sass'
 
-
-const store = configureStore()
 
 ReactDOM.render(
     <Root store={store} />,
