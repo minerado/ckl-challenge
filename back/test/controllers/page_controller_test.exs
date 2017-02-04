de1f13858e7eed87e446bfe37640abc940f66e4f
Unknown <ramondiogo10.1@gmail.com>
2017-02-04 14:30:08 -0200

    Add Test if all posts are returned

--- back/test/controllers/page_controller_test.exs
+++ back/test/controllers/page_controller_test.exs
@@ -1,8 +1,20 @@
 defmodule CklNews.PageControllerTest do
-  use CklNews.ConnCase
+    use CklNews.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
+    test("if all posts are returned on /api/posts", %{conn: conn}) do
+        tech_tag = build(:tag) |> set_tech_tag
+        tech_post = insert(:post, tag: tech_tag)
+        politics_post = insert(:post)
+
+        conn = get conn, "/api/posts"
+        
+        assert json_response(conn, 200) == render_json("index.json", posts: [tech_post, politics_post])
+    end
+    defp render_json(template, assigns) do
+        assigns = Map.new(assigns)
+
+        CklNews.PostView.render(template, assigns)
+            |> Poison.encode!
+            |> Poison.decode!
+    end
 end
