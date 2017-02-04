ba06071e7103954e4cf8bdeea591fa301f424eef
Unknown <ramondiogo10.1@gmail.com>
2017-02-04 14:30:21 -0200

    Add Test if filtered posts are returned

--- back/test/controllers/page_controller_test.exs
+++ back/test/controllers/page_controller_test.exs
@@ -10,6 +10,19 @@ defmodule CklNews.PageControllerTest do
         
         assert json_response(conn, 200) == render_json("index.json", posts: [tech_post, politics_post])
     end
+
+    test("if filtered posts are returned on /api/posts/:filter", %{conn: conn}) do
+        tech_tag = build(:tag) |> set_tech_tag
+        tech_post = insert(:post, tag: tech_tag)
+        politics_post = insert(:post)
+        
+        conn_tech = get conn, "/api/posts/tech"
+        conn_politics = get conn, "/api/posts/politics"
+        
+        assert json_response(conn_tech, 200) == render_json("index.json", posts: [tech_post])
+        assert json_response(conn_politics, 200) == render_json("index.json", posts: [politics_post])
+    end
+
     defp render_json(template, assigns) do
         assigns = Map.new(assigns)
 
